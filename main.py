e8e6853c1404fccd9ea147b2887fc2cf2463f8e3
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-11 09:21:46 -0800

    move forward

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ for index in range(0, 3):
     starting_x = index * -20
     turtle = Turtle()
     turtle.penup()
-    turtle.speed(10)
     turtle.shape("square")
     turtle.color("white")
     turtle.setposition(x=starting_x, y=0)
@@ -25,7 +24,7 @@ def move_forward():
     length = len(turtles)
     for i in range(0, length):
         screen.update()
-        time.sleep(.05)
+        time.sleep(.1)
         if i < length - 1:
             next_turtle = turtles[i + 1]
             next_turtle_position = next_turtle.position()
@@ -35,7 +34,7 @@ def move_forward():
             turtles[i].setposition(x, y)
             turtles[i].setheading(next_turtle_heading)
         else:
-            turtles[i].left(90)
+
             turtles[i].forward(20)
 
 

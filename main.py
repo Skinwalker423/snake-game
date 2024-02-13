2d5ff91be5f54a0f3cce24ae09ff0ce0c3049040
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-12 17:36:38 -0800

    snake methods

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
-
-turtles = []
 
 screen = Screen()
 screen.setup(height=600, width=600)
@@ -11,66 +9,26 @@ screen.title("Snake Game")
 
 new_snake = Snake()
 
-print(new_snake.segments)
-
-for index in range(0, 3):
-    starting_x = index * -20
-    turtle = Turtle()
-    turtle.penup()
-    turtle.shape("square")
-    turtle.color("white")
-    turtle.goto(x=starting_x, y=0)
-    turtles.insert(0, turtle)
-
-
-def move_forward():
-    length = len(turtles)
-    for i in range(0, length):
-        if i < length - 1:
-            next_turtle = turtles[i + 1]
-            next_turtle_position = next_turtle.position()
-            x = next_turtle_position[0]
-            y = next_turtle_position[1]
-            turtles[i].goto(x, y)
-
-        else:
-            turtles[i].forward(20)
-
-
-def turn_left():
-    screen.update()
-    time.sleep(.05)
-    turtles[-1].left(90)
-
-
-def turn_right():
-    screen.update()
-    time.sleep(.05)
-    turtles[-1].right(90)
-
 
 def start_game():
     is_game_on = True
-
     while is_game_on:
         screen.update()
         time.sleep(.1)
-        position = turtles[-1].position()
+        position = new_snake.segments[-1].position()
         position_x = position[0]
         position_y = position[1]
         print(f"position: {position}")
         if position_x > 300 or position_x < -300 or position_y > 300 or position_y < -300:
             is_game_on = False
         else:
-
-            move_forward()
-
+            new_snake.move_forward()
     print("Game Over")
 
 
 screen.onkeypress(key="s", fun=start_game)
-screen.onkey(key="a", fun=turn_left)
-screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="a", fun=new_snake.turn_left)
+screen.onkey(key="d", fun=new_snake.turn_right)
 
 screen.listen()
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -15,3 +15,22 @@ class Snake:
             turtle.color("white")
             turtle.goto(x=starting_x, y=0)
             self.segments.insert(0, turtle)
+
+    def move_forward(self):
+        length = len(self.segments)
+        for i in range(0, length):
+            if i < length - 1:
+                next_turtle = self.segments[i + 1]
+                next_turtle_position = next_turtle.position()
+                x = next_turtle_position[0]
+                y = next_turtle_position[1]
+                self.segments[i].goto(x, y)
+
+            else:
+                self.segments[i].forward(20)
+
+    def turn_left(self):
+        self.segments[-1].left(90)
+
+    def turn_right(self):
+        self.segments[-1].right(90)
